69a7d6617049faa7f3d3e86c2176b6ac611555ea
Leon Maurer <Leon.N.Maurer.08@alum.dartmouth.org>
2008-07-23 22:30:40 -0400

    added license

--- styletool.rb
+++ styletool.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/ruby
 
+#styletool -- a simple word frequency based stylometry tool
+#Copyright (C) 2008  Leon N. Maurer
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#version 2 as published by the Free Software Foundation;
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#A copy of the license is available at 
+#<http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+#You can also receive a paper copy by writing the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 require 'tk'
 require 'yaml' #needs to be before gsl?
 require 'gsl'
